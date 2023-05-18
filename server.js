843a48e4edd7f9bb86df3f0ba0b7c6e450a2f024
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-05-17 21:43:25 -0700

    Added cat and dog routes

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 
 const PORT = 4001;
 
+app.get('/cat', (req, res, next) => {
+    res.send('Cat facts coming soon!');
+})
+
+app.get('/dog', (req, res, next) => {
+    res.send('Dog facts coming soon!');
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
